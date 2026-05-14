7880c01e50be8345cddb0fb42cc8f0b4a78ab919
Bhavin <Bhavinondhiya0@gmail.com>
2026-05-14 12:23:35 +0530

    solved

--- src/parts/BookingWidget.js
+++ src/parts/BookingWidget.js
@@ -1,5 +1,6 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable react/jsx-filename-extension */
+/* eslint-disable import/extensions */
 
 import React from 'react';
 import { Fade } from 'react-awesome-reveal';
